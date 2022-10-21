a4340c12c32d3fde355e41b192500b612d03d206
Mehedi <iammehedi296@gmail.com>
2022-10-21 23:07:27 +0600

    table layout

---
+++ calculator/script.js
@@ -0,0 +1,49 @@
+function sum() {
+    
+    const numOne =parseFloat(document.getElementById("numOne").value)
+
+    const numTwo =parseFloat(document.getElementById("numTwo").value)
+
+    const result=numOne+numTwo;
+
+    alert(result)
+  
+}
+
+function sub() {
+
+    const numOne = parseFloat(document.getElementById("numOne").value)
+
+    const numTwo =parseFloat(document.getElementById("numTwo").value) 
+
+    const result = numOne-numTwo;
+
+    alert(result);
+    
+}
+
+function mul() {
+
+    const numOne = parseFloat(document.getElementById("numOne").value)
+
+    const numTwo =parseFloat(document.getElementById("numTwo").value) 
+
+    const result = numOne*numTwo;
+
+    alert(result);
+    
+    
+}
+
+function div() {
+
+    const numOne = parseFloat(document.getElementById("numOne").value)
+
+    const numTwo =parseFloat(document.getElementById("numTwo").value) 
+
+    const result = numOne/numTwo;
+
+    alert(result);
+    
+    
+}
